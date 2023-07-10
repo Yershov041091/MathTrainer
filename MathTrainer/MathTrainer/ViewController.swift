b2b39a11c25721ab2e16e9bdef1c19dfeb17b627
Artem Yershov <ershov041091@icloud.com>
2023-07-10 19:47:41 +0300

    no message

--- MathTrainer/MathTrainer/ViewController.swift
+++ MathTrainer/MathTrainer/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum MathTypes: Int {
+    case add, substract, multiply, divide
+}
 
+class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet var buttonsCollection: [UIButton]!
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configeButtons()
     }
-
-
+    
+    // MARK: - Actions
+    @IBAction func buttonsAction(_ sender: UIButton) {
+    }
+    
+    // MARK: - Methods
+    private func configeButtons() {
+        buttonsCollection.forEach { button in
+            button.layer.shadowColor = UIColor.gray.cgColor
+            button.layer.shadowOffset = CGSize(width: 3, height: 5)
+            button.layer.shadowOpacity = 0.6
+            button.layer.shadowRadius = 4
+        }
+    }
 }
 
